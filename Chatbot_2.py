fef371c7a6dc29ced72e9b9c2043af456f116d55
ArtificiallyAndrew <33381625+ArtificiallyAndrew@users.noreply.github.com>
2024-03-27 11:20:24 -0400

    Update Chatbot_2.py

--- Chatbot_2.py
+++ Chatbot_2.py
@@ -30,6 +30,7 @@ st.image(image_path,  width=100)
 image_path = "PathwaysLogo.png"
 website_url = "https://pathways.facultyadvance.org/"
 st.markdown(f'[![Image](/{image_path})]({website_url})', unsafe_allow_html=True)
+st.markdown(f'<a href="{website_url}" target="_blank"><img src="{image_path}" width="100"></a>', unsafe_allow_html=True)
 
 st.title("Welcome to the Pathways Navigator")
 st.markdown("""
